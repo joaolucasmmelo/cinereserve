341b67c2b0211e5a7c43eb35af0303115c1b9e46
João Lucas de Melo <joaolucasmm2512@gmail.com>
2026-03-16 16:09:25 -0300

    Impement user registration api. Next thing will be the JWT tokens

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all
+    serializer_class = UserSerializer
 # Create your views here.
